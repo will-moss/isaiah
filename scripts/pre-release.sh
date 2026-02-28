4875806d3fa3ed0e4244072f08588f305681cf7d
Will Moss <contact@will-moss.me>
2026-03-01 00:53:17 +0100

    fix(build): fixed the build steps to match the newer babel/lightiningcss and PNPM configurations

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -7,19 +7,19 @@ cd ./app/
 go mod tidy
 
 # Compile LESS files into one unique CSS file
-npx --yes lessc ./client/assets/css/style.less > ./client/assets/css/tmp.css
+pnpx less ./client/assets/css/style.less > ./client/assets/css/tmp.css
 
 # Minify and Prefix CSS
-npx --yes lightningcss --minify --bundle --targets 'cover 99.5%' ./client/assets/css/tmp.css -o ./client/assets/css/style.css
+pnpx lightningcss-cli --minify --bundle --targets 'cover 99.5%' ./client/assets/css/tmp.css -o ./client/assets/css/style.css
 
 # Save the original JS file
 cp ./client/assets/js/isaiah.js ./client/assets/js/isaiah.backup.js
 
 # Make JS cross-browser-compatible
-npx --yes babel ./client/assets/js/isaiah.js --out-file ./client/assets/js/isaiah.js --config-file ./.babelrc.json
+npx babel ./client/assets/js/isaiah.js --out-file ./client/assets/js/isaiah.js --config-file ./.babelrc.json
 
 # Minify JS
-npx --yes terser ./client/assets/js/isaiah.js -o ./client/assets/js/isaiah.js
+pnpx terser ./client/assets/js/isaiah.js -o ./client/assets/js/isaiah.js
 
 # Append a version parameter to the main JS & CSS linked files to prevent caching
 VERSION=$(git describe --tags --abbrev=0)
